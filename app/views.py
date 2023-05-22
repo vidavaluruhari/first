c07034605e5372f69afd6f2de7793bc6af1f4891
vidavaluruhari <vidavaluruhari1234@gmail.com>
2023-05-22 11:36:59 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def sree(request):
+    return HttpResponse('<h1><marquee>vishnu pulihora mixing king</marquee></h1>')
